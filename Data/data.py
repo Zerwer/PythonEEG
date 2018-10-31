eb3ddeea4e1c3b45c03e774fcc87fe5f1862d8cf
Aidan Zerwer <robobug9@gmail.com>
2018-10-31 14:05:41 -0400

    Comments

--- Data/data.py
+++ Data/data.py
@@ -1,8 +1,10 @@
 import serial
 
+# Connects to HC-06 at a baudrate of 57600
 s = serial.Serial(port='/dev/tty.HC-06-DevB', baudrate=57600)
 
+# Loop of collecting information
 while True:
-    size = s.inWaiting()
+    size = s.inWaiting()  # Returns int of packets waiting to be read
     if size:
-        print(int.from_bytes(s.read(size), byteorder='big'))
+        print(int.from_bytes(s.read(size), byteorder='big'))  # Convert read bytes to integer using big byte order
