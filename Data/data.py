caed0cb94ff4034ccbb4a51b475aebf0765aa71a
Aidan Zerwer <robobug9@gmail.com>
2018-10-31 22:03:07 -0400

    rm dumb stuff

--- Data/data.py
+++ Data/data.py
@@ -1,13 +1,15 @@
 import serial
 
+device = '/dev/tty.HC-06-DevB'
+
 # Initialization of HC-06 at a baud rate of 9600(default) sending 0x02 to initiate
-s = serial.Serial(port='/dev/tty.HC-06-DevB', baudrate=9600)
+s = serial.Serial(port=device, baudrate=9600)
 s.write(b'\x02')  # Must be sent in bytes
 s.close()
 
 
 # Connects to HC-06 at a baudrate of 57600
-s = serial.Serial(port='/dev/tty.HC-06-DevB', baudrate=57600)
+s = serial.Serial(port=device, baudrate=57600)
 
 file = open('out.bin', 'wb')
 
@@ -15,6 +17,5 @@ file = open('out.bin', 'wb')
 while True:
     size = s.inWaiting()  # Returns int of packets waiting to be read
     if size:
-        #print(int.from_bytes(s.read(size), byteorder='big'))  # Convert read bytes to integer using big byte order
         print(s.read(size).hex())
         file.write(s.read(size))
