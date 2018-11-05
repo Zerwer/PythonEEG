6a8527c40c4fcf0d807c8f437c279dd5f56efce1
Aidan Zerwer <robobug9@gmail.com>
2018-11-05 14:18:43 -0500

    Graphing stuff

--- Data/data.py
+++ Data/data.py
@@ -1,22 +1,23 @@
-"""
-Packet dissection:
-aa - start (static)
-048002 - key (dynamic) changes each session
-ff7905 - data (dynamic)
-aa - end (static)
-
-keys:
-068204
-048002
-"""
+# Packet dissection:
+# aa - start (static)
+# 048002 - key (dynamic) changes each session
+# ff7905 - data (dynamic)
+# aa - end (static)
+#
+# keys:
+# 068204
+# 048002
 import serial
+import time
 
 # Device used
 device = '/dev/tty.HC-06-DevB'
 
-# Initialization of HC-06 at a baud rate of 9600(default) sending 0x02 to initiate
+# Initialization of HC-06 at a baud rate of 9600(default) sending 0x02 to initiate (twice for certainty)
 s = serial.Serial(port=device, baudrate=9600)
 s.write(b'\x02')  # Must be sent in bytes
+time.sleep(1)
+s.write(b'\x02')
 s.close()
 
 
@@ -25,6 +26,7 @@ s = serial.Serial(port=device, baudrate=57600)
 
 # Open file to output to
 file = open('out.bin', 'wb')
+file2 = open('data.out', 'w')
 
 # Declare blank storage packet
 packet = ''
@@ -34,7 +36,9 @@ while True:
     data = s.read(1)
     if data.hex() == 'aa':
         if packet != '':
+            # print(int(packet[-6::], 16))
             print(packet)
+            file2.write(str(int(packet[-6::], 16))+'\n')
             packet = ''
     else:
         packet = packet+data.hex()
--- Data/graph.py
+++ Data/graph.py
@@ -3,4 +3,31 @@ from tkinter import *
 root = Tk()
 root.geometry('900x556')  # Golden ratio
 root.title('EEG Raw Data')
+
+graphing_area = Canvas(root, width=900, height=556)
+graphing_area.pack()
+
+points = []
+
+file = open('data.out', 'r')
+lines = file.readlines()
+
+
+def draw_point(points):
+    i = 0
+    for point in points:
+        graphing_area.create_rectangle(i, (int(point)/99000)*556, i, (int(point)/99000)*556)
+        i += 1
+
+
+for line in lines:
+    if int(line) > 99000: lines.remove(line)
+
+stuff = []
+
+for i in range(900):
+    stuff.append(lines[i])
+
+draw_point(stuff)
+
 root.mainloop()
