31b1dd7245267346639d65d797949a1f4abf175d
Aidan Zerwer <robobug9@gmail.com>
2018-11-23 17:16:24 -0500

    Graph scale

--- data/graphics.py
+++ data/graphics.py
@@ -10,9 +10,22 @@ graphing_area = Canvas(root, width=900, height=556)
 graphing_area.pack()
 
 
+def scale():
+    graphing_area.create_text(450, 530, text='Time (Seconds)', anchor='center', font="Arial 20 bold")
+    graphing_area.create_rectangle(849, 480, 847, 450, fill='black')
+    graphing_area.create_text(848, 430, text='1', anchor='center', font="Arial 20 bold")
+    graphing_area.create_rectangle(645, 480, 643, 450, fill='black')
+    graphing_area.create_text(644, 430, text='5', anchor='center', font="Arial 20 bold")
+    graphing_area.create_rectangle(389, 480, 387, 450, fill='black')
+    graphing_area.create_text(388, 430, text='10', anchor='center', font="Arial 20 bold")
+    graphing_area.create_rectangle(135, 480, 133, 450, fill='black')
+    graphing_area.create_text(134, 430, text='15', anchor='center', font="Arial 20 bold")
+
+
 # Function called to redraw points
 def draw_point():
     graphing_area.delete('all')  # Clear every redraw
+    scale()
 
     i = 0
     for point in y_values:
--- training/training.py
+++ training/training.py
@@ -3,32 +3,38 @@ import os
 from keras.layers import Dense, Flatten, Dropout
 from keras.models import Sequential
 
+# Declare blank sets
 x_train = []
 y_train = []
 
 x_valid = []
 y_valid = []
 
+# Prepare training set
 for file in os.listdir('letters'):
     if file[:1] == 'a':
-        y_train.append(0)
-    else:
         y_train.append(1)
+    else:
+        y_train.append(0)
+    # Use fft and collect real numbers divided by 85000 to get input float
     x_train.append(np.abs(np.fft.rfft(np.genfromtxt('letters/'+file, delimiter=',')))/85000)
 
 x_train = np.expand_dims(np.array(x_train), 3)
 y_train = np.array(y_train)
 
+# Prepare validation set
 for file in os.listdir('validation'):
     if file[:1] == 'a':
-        y_valid.append(0)
-    else:
         y_valid.append(1)
+    else:
+        y_valid.append(0)
+    # Use fft and collect real numbers divided by 85000 to get input float
     x_valid.append(np.abs(np.fft.rfft(np.genfromtxt('validation/'+file, delimiter=',')))/85000)
 
 x_valid = np.expand_dims(np.array(x_valid), 3)
 y_valid = np.array(y_valid)
 
+# Create model
 model = Sequential([
     Flatten(input_shape=(751, 1)),
     Dense(128, activation='relu'),
@@ -40,5 +46,6 @@ model = Sequential([
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, validation_data=(x_valid, y_valid), shuffle=True, batch_size=30, epochs=1000)
+model.fit(x_train, y_train, validation_data=(x_valid, y_valid), shuffle=True, batch_size=30, epochs=200)
+
 model.save('model.h5')
